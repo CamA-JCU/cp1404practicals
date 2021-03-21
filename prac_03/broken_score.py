87710636e9eec48e825e796286674cbf4fa0e6d3
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-03-21 15:58:17 +1000

    Refactor broken score program and temperatures program using functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,13 +3,29 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+import random
+
+
+def main():
+    """Score Status Program"""
     score = float(input("Enter score: "))
-if score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+    result = determine_result(random.randint(0,101))
+    print(result)
+
+
+def determine_result(score):
+    """Determine result from score"""
+    if score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
