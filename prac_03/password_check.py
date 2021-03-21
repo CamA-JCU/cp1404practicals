24745da8683e6845a5fe7a92c759e29755bdcb67
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-03-21 15:43:15 +1000

    Refactor password program into functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -5,9 +5,24 @@ Cameron Attard
 
 MIN_LENGTH = 8
 
-password = str(input("Enter password that is {} in length: ".format(MIN_LENGTH)))
-while len(password) < MIN_LENGTH:
-    print("Password does not meet minimum length requirements")
-    password = str(input("Enter password that is {} in length: ".format(MIN_LENGTH)))
+def main():
+    """Password Checker Program"""
+    password = get_password()
+    print_password(password)
 
-print("*" * len(password))
+
+def print_password(password):
+    """Print password using asterisks"""
+    print("*" * len(password))
+
+
+def get_password():
+    """Get Password from user"""
+    password = str(input("Enter password that is {} in length: ".format(MIN_LENGTH)))
+    while len(password) < MIN_LENGTH:
+        print("Password does not meet minimum length requirements")
+        password = str(input("Enter password that is {} in length: ".format(MIN_LENGTH)))
+    return password
+
+
+main()
