5788ab147d0e0674f0385775da2c3772fb359e83
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-03-28 17:07:18 +1000

    Add lists_warmup program Add total_income program starter code

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+"""
+CP1404 - prac_04 - lists_warmup.py
+Cameron Attard
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
+
+# 2. Change the last element of numbers to 1
+numbers[-1] = 1
+
+# 3. Get all the elements from numbers except the first two (slice)
+numbers [2:]
+
+# 4. Check if 9 is an element of numbers
+9 in numbers
+
