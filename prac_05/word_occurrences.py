93a7b66b6e6a5ce4865b5b19ba01e9cfd9320b27
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-04-05 21:44:10 +1000

    Write program using test input with unsorted output

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,5 +1,18 @@
 """
 CP1404/CP5632 Practical
-A program to count the occurrences of words in a string.
+Count the occurrences of words in a string.
 """
 
+words_to_count = {}
+# text = input("Text: ")
+text = "this is a collection of words of nice words this is a fun thing it is"
+words = text.split()
+for word in words:
+    try:
+        words_to_count[word] += 1
+    except KeyError:
+        words_to_count[word] = 1
+
+# print each word with it's word count
+for word in words_to_count:
+    print(f"{word} : {words_to_count[word]}")
