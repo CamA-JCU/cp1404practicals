e326cfce5be2267942fea7f365069e3edca91991
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-04-18 22:39:58 +1000

    Update ProgrammingLanguage class to have name field

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,12 +4,17 @@
 class ProgrammingLanguage:
     """Represent a Programming Language."""
 
-    def __init__(self, typing="", reflection="", year=0):
+    def __init__(self, name="", typing="", reflection="", year=0):
         """Initialise a Programming Language instance."""
-
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        """Return string representation of ProgrammingLanguage object."""
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(
+            self.name, self.typing, self.is_dynamic(), self.year)
 
     def is_dynamic(self):
         """Determine if language is dyanmic."""
