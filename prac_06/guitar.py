cae28d96e4b085eaa2fb15bcb1d623c13746d119
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-04-18 22:52:04 +1000

    Create __init__, __str__, get_age and is_vintage methods for Guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,2 +1,27 @@
 """CP1404/CP5632 Practical - Guitar class."""
 
+CURRENT_YEAR = 2021
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of Guitar object."""
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        """Return the age of the Guitar object."""
+        age = CURRENT_YEAR - self.year
+        return age
+
+    def is_vintage(self):
+        """Determine if the Guitar is vintage."""
+        return self.get_age() >= VINTAGE_AGE
