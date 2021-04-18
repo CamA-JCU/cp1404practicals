adefcbc04ec3bcdd9b438318eae7181b6af60b5d
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-04-18 23:02:35 +1000

    Initial commit of guitars.py program

---
+++ prac_06/guitars.py
@@ -0,0 +1,11 @@
+"""CP1404/CP5632 Practical - Client code to use the Guitar class."""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Client code to use the Guitar class."""
+
+
+if __name__ == '__main__':
+    main()
