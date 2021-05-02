71eb1e54533e14b4e51f62f55737ecf40a903582
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-02 19:16:25 +1000

    Add docstring to program fucntions

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -10,23 +10,27 @@ MILES_TO_KM = 1.609344
 
 
 class ConvertMilesToKMProgram(App):
-
+    """ConvertMilesToKMProgram is a KIVY GUI program for converting miles to km."""
     def build(self):
+        """Build app using Kivy app from kv file"""
         self.title = "Convert Miles to KM Program"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def calculate_miles_to_km(self):
+        """Calculates miles to km and outputs to GUI"""
         miles = self.get_valid_miles()
         km = miles * MILES_TO_KM
         self.root.ids.km_output.text = str(km)
 
     def handle_increment(self, increment):
+        """Handle increment buttons both up and down"""
         new_miles = self.get_valid_miles() + increment
         self.root.ids.miles_input.text = str(new_miles)
         self.calculate_miles_to_km()
 
     def get_valid_miles(self):
+        """Get valid miles from input text"""
         try:
             miles = float(self.root.ids.miles_input.text)
             return miles
