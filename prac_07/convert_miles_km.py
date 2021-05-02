6bd03a45b27417cbd9d7a9dbd41ac59f70c6a578
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-02 19:10:47 +1000

    Create handle functions for calculate button and incremental buttons

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -5,22 +5,30 @@ Cameron Attard
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import StringProperty
 
 MILES_TO_KM = 1.609344
 
 
 class ConvertMilesToKMProgram(App):
 
-    km = StringProperty()
-
     def build(self):
         self.title = "Convert Miles to KM Program"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_press(self):
-        self.km = self.root.ids.miles_input.text
+    def calculate_miles_to_km(self):
+        miles = self.get_valid_miles()
+        km = miles * MILES_TO_KM
+        self.root.ids.km_output.text = str(km)
+
+    def handle_increment(self, increment):
+        new_miles = self.get_valid_miles() + increment
+        self.root.ids.miles_input.text = str(new_miles)
+        self.calculate_miles_to_km()
+
+    def get_valid_miles(self):
+        miles = float(self.root.ids.miles_input.text)
+        return miles
 
 
 ConvertMilesToKMProgram().run()
