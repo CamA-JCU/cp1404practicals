b363811e8f5bb44c97a224e1198e6f19ddb271a6
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-08 19:12:53 +1000

    Complete UnreliableCar class

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -4,3 +4,22 @@ Car class
 Cameron Attard
 """
 
+from prac_08.car import Car
+import random
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car that includes reliability."""
+
+    def __init__(self, name, fuel, reliability=0):
+        """Initialise a UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive like parent Car but only if randomly generated number is less than Car's reliability."""
+        if random.randint(0, 100) < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0
+        return distance_driven
