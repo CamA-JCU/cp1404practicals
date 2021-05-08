5a3ded8d6930c88ef879d9eca874cc04e7d2d6ba
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-08 21:03:24 +1000

    Create choose valid taxi function

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -12,6 +12,7 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     """Taxi Simulator."""
     current_taxi = None
+    bill_to_date = 0.00
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     print(MENU)
@@ -20,12 +21,26 @@ def main():
         if menu_choice == "D":  # Drive
             print("Drive")
         elif menu_choice == "C":  # Choose Taxi
-            print("Choose Taxi")
+            # print("Choose Taxi")
+            current_taxi = choose_valid_taxi(taxis)
+            # print(current_taxi)
         else:
             print("Invalid Option")
         print(MENU)
         menu_choice = input(">>> ").upper()
     print("Quit")
+
+
+def choose_valid_taxi(taxis):
+    """Choose a valid taxi from Taxis"""
+    print("Taxis available:")
+    for i, taxi in enumerate(taxis):
+        print("{} - ".format(i), taxi)
+    selected_taxi = int(input("Choose taxi: "))
+    if selected_taxi >= len(taxis) or selected_taxi < 0:
+        print("Invalid taxi choice")
+        selected_taxi = None
+    return selected_taxi
 
 
 if __name__ == '__main__':
