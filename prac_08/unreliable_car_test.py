fa0ae390f2edd47b94e9921a5e2917743aca2b79
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-08 19:16:11 +1000

    Complete test program of UnreliableCar class

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -2,3 +2,12 @@
 CP1404/CP5632 Prac_08 unreliable_car_test.py
 Cameron Attard
 """
+
+from prac_08.unreliable_car import UnreliableCar
+
+unreliable_car = UnreliableCar("Kia 1", 100, 40)
+print(unreliable_car)
+unreliable_car.drive(40)
+print(unreliable_car)
+unreliable_car.drive(50)
+print(unreliable_car)
