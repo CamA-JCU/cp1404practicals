80a77ba9f13761d8e4cae3036cc1cf42d2711d4d
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-08 18:58:48 +1000

    Add code for first 5 steps of Test Taxi program

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,3 +2,14 @@
 CP1404/CP5632 Prac_08 taxi_text.py
 Cameron Attard
 """
+
+from prac_08.taxi import Taxi
+
+prius_1 = Taxi("Prius 1", 100, 1.23)
+prius_1.drive(40)
+print(prius_1)
+print("Current fare: ${}".format(prius_1.get_fare()))
+prius_1.start_fare()
+prius_1.drive(100)
+print(prius_1)
+print("Current fare: ${}".format(prius_1.get_fare()))
