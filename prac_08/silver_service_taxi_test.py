443c65388203cfeba60481a40d43bc778a80185e
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-08 20:22:29 +1000

    Complete Silver Service Taxi test program

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -2,3 +2,12 @@
 CP1404/CP5632 Prac_08 silver_service_taxi_test.py
 Cameron Attard
 """
+
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+hummer = SilverServiceTaxi("Hummer", 200, 2)
+print(hummer)
+hummer.drive(18)
+print(hummer)
+print(hummer.get_fare())
