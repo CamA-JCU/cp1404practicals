e88a790844a3ad3df58cb716c27f79352d1503b5
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-08 21:28:08 +1000

    Create take_taxi_trip function and print_available_taxis function and print statements when quitting program

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -19,28 +19,51 @@ def main():
     menu_choice = input(">>> ").upper()
     while menu_choice != "Q":  # Quit
         if menu_choice == "D":  # Drive
-            print("Drive")
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                taxi = taxis[current_taxi]
+                trip_fare = take_taxi_trip(taxi)
+                bill_to_date += trip_fare
+            print("Bill to date: ${:.2f}".format(bill_to_date))
         elif menu_choice == "C":  # Choose Taxi
             # print("Choose Taxi")
             current_taxi = choose_valid_taxi(taxis)
+            print("Bill to date: ${:.2f}".format(bill_to_date))
             # print(current_taxi)
         else:
             print("Invalid Option")
         print(MENU)
         menu_choice = input(">>> ").upper()
-    print("Quit")
+    print("Total trip cost: ${:.2f}".format(bill_to_date))
+    print("Taxis are now:")
+    print_available_taxis(taxis)
+
+
+def take_taxi_trip(taxi):
+    distance = float(input("Drive how far? "))
+    taxi.start_fare()
+    taxi.drive(distance)
+    current_fare = taxi.get_fare()
+    print("Your {} trip cost you ${:.2f}".format(taxi.name, current_fare))
+    return current_fare
 
 
 def choose_valid_taxi(taxis):
     """Choose a valid taxi from Taxis"""
     print("Taxis available:")
-    for i, taxi in enumerate(taxis):
-        print("{} - ".format(i), taxi)
+    print_available_taxis(taxis)
     selected_taxi = int(input("Choose taxi: "))
     if selected_taxi >= len(taxis) or selected_taxi < 0:
         print("Invalid taxi choice")
         selected_taxi = None
     return selected_taxi
+
+
+def print_available_taxis(taxis):
+    """Print a list of available taxis"""
+    for i, taxi in enumerate(taxis):
+        print("{} - ".format(i), taxi)
 
 
 if __name__ == '__main__':
