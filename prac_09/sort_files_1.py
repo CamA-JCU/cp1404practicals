2d7069bab03073251cfdb3725040451ed29ece9c
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-15 14:19:48 +1000

    Find extensions and append to extensions list

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -3,3 +3,22 @@ CP1404/CP5632 Practical
 Sort Files Program V1.0
 """
 
+import os
+import shutil
+
+
+def main():
+    """Sort Files Program V1.0."""
+    os.chdir('FilesToSort')
+    extensions = []
+    for filename in os.listdir('.'):
+        print(filename)
+        extension_starts = filename.find(".") + 1
+        print(extension_starts)
+        extension = filename[extension_starts:]
+        print(extension)
+        extensions.append(extension)
+    print(extensions)
+
+
+main()
