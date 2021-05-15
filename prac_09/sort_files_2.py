c916a26d3003e6513b5f96e4a96d68c52d6b28a3
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-15 14:41:36 +1000

    Initial commit of sort_files_2.py program

---
+++ prac_09/sort_files_2.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 Practical
+Sort Files Program V2.0
+"""
+
+import os
+import shutil
+
+
+def main():
+    """Sort Files Program V2.0."""
+    os.chdir('FilesToSort')
+    extensions = []
+    for filename in os.listdir('.'):
+        pass
+
+
+main()
