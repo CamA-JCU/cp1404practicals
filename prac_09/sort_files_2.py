1b072d829aca43f79eea7ceed1aa04f100cb1edf
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-15 15:01:29 +1000

    Edit program to categorise extensions

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -10,9 +10,16 @@ import shutil
 def main():
     """Sort Files Program V2.0."""
     os.chdir('FilesToSort')
-    extensions = []
+    extension_to_category = {}
     for filename in os.listdir('.'):
-        pass
+        # find files that don't start with "."
+        if not filename[0] == ".":
+            extension = filename.split(".")[-1]
+            # print(extension)
+            if extension not in extension_to_category:
+                category = input("What category would you like to sort {} files into? ".format(extension))
+                extension_to_category[extension] = category
+    # print(extension_to_category)
 
 
 main()
