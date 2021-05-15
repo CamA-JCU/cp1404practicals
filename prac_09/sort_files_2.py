6bc5ad4e1de83d9ae2a7ca35cd3bc8df213c6345
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-15 15:09:55 +1000

    Remove commented testing lines from code

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -15,7 +15,6 @@ def main():
         # find files that don't start with "."
         if not filename[0] == ".":
             extension = filename.split(".")[-1]
-            # print(extension)
             if extension not in extension_to_category:
                 category = input("What category would you like to sort {} files into? ".format(extension))
                 try:
@@ -24,12 +23,9 @@ def main():
                     pass
                 extension_to_category[extension] = category
                 shutil.move(filename, category + '/' + filename)
-                # print("Move {} to {}".format(filename, category))
             else:
                 category = extension_to_category[extension]
-                # print("Move {} to {}".format(filename, category))
                 shutil.move(filename, category + '/' + filename)
-    # print(extension_to_category)
 
 
 main()
