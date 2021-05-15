b406f757d2d74043f84a5beba41d4dca19933347
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-05-15 15:09:16 +1000

    Complete sort_files_2.py program

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -18,7 +18,17 @@ def main():
             # print(extension)
             if extension not in extension_to_category:
                 category = input("What category would you like to sort {} files into? ".format(extension))
+                try:
+                    os.mkdir(category)
+                except FileExistsError:
+                    pass
                 extension_to_category[extension] = category
+                shutil.move(filename, category + '/' + filename)
+                # print("Move {} to {}".format(filename, category))
+            else:
+                category = extension_to_category[extension]
+                # print("Move {} to {}".format(filename, category))
+                shutil.move(filename, category + '/' + filename)
     # print(extension_to_category)
 
 
